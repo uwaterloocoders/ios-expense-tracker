2446e9bd4f664409e81d3da720f26e70fba4352e
Kushal Goel <64199835+kushalgoel786@users.noreply.github.com>
2023-01-13 23:09:16 -0500

    Committed Initial App

---
+++ expensetracker/expensetracker/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  trial
+//
+//  Created by Kushal Goel on 13/01/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ expensetracker/expensetracker/expensetrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  trialApp.swift
+//  trial
+//
+//  Created by Kushal Goel on 13/01/23.
+//
+
+import SwiftUI
+
+@main
+struct trialApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
